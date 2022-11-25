ba8567293ad088c9bb72a9c93877ac53dea60efe
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-25 10:28:07 +0000

    Adding tarfile member sanitization to extractall()

--- data/x-ray-data/downloader.py
+++ data/x-ray-data/downloader.py
@@ -23,7 +23,29 @@ for idx, link in enumerate(links):
     
     with tarfile.open(fn) as zipped_file:
         print(f"Uncompressing {fn} ...")
-        zipped_file.extractall('data')
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(zipped_file, "data")
     
     print("\n")
 
